46bad214950aef5fb0c9ea47700463fce479b69b
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-01-18 19:01:58 +0600

    filter product and search

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+const search = () => {
+  const searchBox = document.getElementById("search-item").value.toUpperCase();
+  const product = document.querySelectorAll(".product");
+  const pname = document.querySelectorAll("h2");
+
+  for (let i = 0; i < pname.length; i++) {
+    let match = product[i].getElementsByTagName("h2")[0];
+
+    if (match) {
+      let textValue = match.textContent || match.innerHTML;
+
+      if (textValue.toUpperCase().indexOf(searchBox) > -1) {
+        product[i].style.display = "";
+      } else {
+        product[i].style.display = "none";
+      }
+    }
+  }
+};
